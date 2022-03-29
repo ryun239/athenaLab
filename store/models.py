d9c1b1e6ce08f892563f492d3addc73b73164098
sanggu239 <romensch39@naver.com>
2022-03-29 21:44:23 +0900

    1. test

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
 from django.db import models
 
+
 # Create your models here.
+class Stock(models.Model):
+    Idx = models.IntegerField()
+    meterial = models.CharField(max_length=32)
+    quntity = models.IntegerField()
---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django import views
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.test_url),
+]
